1209b057a8ecef04023e6aed9c44212b1edaa5c9
Ira Horecka <ira89@icloud.com>
2021-11-15 13:17:55 -0500

    update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,7 +4,7 @@
 crontab -l > mycron
 # Setup cron headers
 echo "SHELL=/bin/bash" >> mycron
-# For executing updating IG bio every hour every day.
-echo "0 */1 * * * source ~/.bashrc && source /home/pi/instagram-bio-updater/venv/bin/activate && bash run.sh" >> mycron
+# For executing updating IG bio every hour every day on the RaspberryPi.
+echo "0 */1 * * * source ~/.bashrc && source /home/pi/Desktop/instagram-bio-updater/venv/bin/activate && bash /home/pi/Desktop/instagram-bio-updater/run.sh" >> mycron
 crontab mycron
 rm mycron
